c0c178f18174c996bb628fce7f5e035de1c59573
Violet Stanziani <stanzianiviolet@gmail.com>
2024-08-08 12:14:16 -0400

    finished model index.js

--- server/models/craft.js
+++ server/models/craft.js
@@ -6,6 +6,10 @@ const craftSchema = new Schema(
         type: String, 
         required : true,
     },
+    description: {
+        type: String, 
+        required: true
+    },
     projects : [
         {
 
--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,5 @@
-//index.js
+const User = require('./user');
+const Project = require('./project');
+const Craft = require('./craft');
+
+module.exports = {User, Project, Craft};
