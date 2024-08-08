6922e73973f049bcf7efa34cc6517c41ed2d6400
Megan McNeill <mcneim2@gmail.com>
2024-08-08 15:13:37 -0400

    commenting out header placeholders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { Header } from './components/Header';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -11,6 +12,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Header />
         <Outlet />
       </div>
     </ApolloProvider>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,10 @@
+// import { useState } from "react";
+// get loggedin and quiz values from...
+
 export default function Header() {
-    // get loggedin and quiz values
-    
+    // const [ loggedIn, setloggedIn ] = useState();
+    // const [ quiz, setQuiz ] = useState();
+
     return (
         <header className="bg-white">
             <nav
@@ -13,15 +17,16 @@ export default function Header() {
 
                 <div className="">
                     {/* // login or signup button if user is not logged in */}
-                    {!loggedIn && <a className="btn btn-primary" href="#" role="button">Login/Signup</a>}
+                    {/* {!loggedIn && <a className="btn btn-primary" href="#" role="button">Login/Signup</a>} */}
+                    <a className="btn btn-primary" href="#" role="button">Login/Signup</a>
                     {/* // logout button if user is logged in */}
-                    {loggedIn && <a className="btn btn-primary" href="#" role="button">Logout</a>}
+                    {/* {loggedIn && <a className="btn btn-primary" href="#" role="button">Logout</a>} */}
                     {/* // dashboard if user is logged in and has completed the quiz*/}
-                    {loggedIn && quiz && <a className="btn btn-primary" href="#" role="button">Dashboard</a>}
+                    {/* {loggedIn && quiz && <a className="btn btn-primary" href="#" role="button">Dashboard</a>} */}
                     {/* // explore if user is logged in and has completed the quiz */}
-                    {loggedIn && quiz && <a className="btn btn-primary" href="#" role="button">Explore</a>}
+                    {/* {loggedIn && quiz && <a className="btn btn-primary" href="#" role="button">Explore</a>} */}
                     {/* // quiz if user is logged in and has not completed the quiz */}
-                    {loggedIn && !quiz && <a className="btn btn-primary" href="#" role="button">Quiz</a>}
+                    {/* {loggedIn && !quiz && <a className="btn btn-primary" href="#" role="button">Quiz</a>} */}
 
                 </div>
             </nav>
