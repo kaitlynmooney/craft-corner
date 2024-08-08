f398afaaf7a4ae952a48de2ba21d9b97d1612f53
kaitlynmooney <kmooney806@gmail.com>
2024-08-08 14:08:56 -0400

    added hello world to home.jsx

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_MATCHUPS } from '../utils/queries';
 
 // Button links to About and Login/Sign Up
 
+const Home = () => {
+return (
+
 <h1>Hello World</h1>
 
+)};
 
 // Landing Page Video
+
+export default Home;
