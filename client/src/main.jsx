8c3cea1a4ee7881ee90f720e81bab349ec8ac347
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-08-08 11:08:41 -0500

    fix: adds resolvers and typedefs to allow server to run

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,24 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+/* DEPENDENCIES */
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import App from "./App.jsx";
+// import Login from "./pages/Login";
 
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        // element: <Login />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
