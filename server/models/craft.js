6d59bc85301afd5396664bb2dbf24688747e1b09
Violet Stanziani <stanzianiviolet@gmail.com>
2024-08-08 11:54:06 -0400

    added craft model

--- server/models/craft.js
+++ server/models/craft.js
@@ -1,1 +1,20 @@
-//craft models
+const { Schema, model} = require('mongoose');
+
+const craftSchema = new Schema(
+    {
+    name: {
+        type: String, 
+        required : true,
+    },
+    projects : [
+        {
+
+            type: Schema.Types.ObjectId,
+            ref: "Project"
+        }
+    ]
+});
+
+const Craft = model('Craft', craftSchema);
+
+module.exports = Craft;
