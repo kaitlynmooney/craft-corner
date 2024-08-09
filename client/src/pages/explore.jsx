ae860f0d840d03f3df63c83fd12ba2dac49f0074
kaitlynmooney <kmooney806@gmail.com>
2024-08-09 12:27:38 -0400

    added base styling for explore page

--- client/src/pages/explore.jsx
+++ client/src/pages/explore.jsx
@@ -1,7 +1,14 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 const Explore = () => {
-
-}
+  return (
+    <div id="explore">
+        <p>Featured Craft of the Week</p>
+        <div>
+        <h1 id="featured-craft">Flower Pots</h1>
+        </div>
+    </div>
+  );
+};
 
 export default Explore;
