465dee0474e39f542dcf447b2677540c0fd495b3
Megan McNeill <mcneim2@gmail.com>
2024-08-08 21:59:55 -0400

    added user related typedefs and mutations

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,9 +1,29 @@
 // /* TYPEDEFS */
 // /* fix later on */
 const typeDefs = `
-  type Query {
-    me: String
-  }
+
+type User {
+  _id: ID
+  username: String
+  email: String
+  password: String
+}
+  
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Query {
+  users: [User]
+  user(username: String!): User
+  me: User
+}
+
+type Mutation {
+  addUser(username: String!, email: String!, password: String!): Auth
+  login(email: String!, password: String!): Auth
+}
 `;
 
 /* EXPORT */
