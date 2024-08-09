258ca0561bbbfd473cddec7bc567e620b9fe8252
Megan McNeill <mcneim2@gmail.com>
2024-08-08 22:27:47 -0400

    added user queries

--- client/src/utils/queires.js
+++ client/src/utils/queires.js
@@ -1,1 +1,32 @@
-//add queries
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      thoughts {
+        _id
+        thoughtText
+        thoughtAuthor
+        createdAt
+      }
+    }
+  }
+`;
