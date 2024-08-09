d3ac1c4d5bc2e556696acc8a9843529b6f06a300
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-08-09 11:38:56 -0500

    feat: error page

--- client/src/pages/error.jsx
+++ client/src/pages/error.jsx
@@ -3,47 +3,24 @@ import React from "react";
 
 /* STYLES */
 const styles = {
-  background: {
-    background: "var(--clear)",
-    position: "relative",
-    width: "100vw",
-    height: "100vh",
-    overflow: "hidden",
-  },
-  iframe: {
-    background: "var(--clear)",
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    border: "none",
-    pointerEvents: "none",
-    zIndex: "-1",
-  },
   heading: {
     position: "absolute",
-    top: "50%",
+    top: "48%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    color: "white",
+    color: "var(--text-color)",
     fontSize: "3rem",
     textAlign: "center",
     zIndex: "1",
+    fontWeight: "300",
   },
 };
 
 /* ERROR PAGE */
 const Error = () => {
   return (
-    <div style={styles.background}>
-      <iframe
-        src="https://player.vimeo.com/video/996390689?autoplay=1&loop=1&background=1"
-        allow="autoplay; fullscreen"
-        title="painting"
-        style={styles.iframe}
-      ></iframe>
-      <h1 className="sofia" style={styles.heading}>
+    <div>
+      <h1 className="inter" style={styles.heading}>
         This page doesn't exist!
       </h1>
     </div>
