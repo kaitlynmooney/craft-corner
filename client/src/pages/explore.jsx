b962a2a0b77d4c936508caf47ab1a8a5de49c72f
kaitlynmooney <kmooney806@gmail.com>
2024-08-09 14:14:33 -0400

    imported dropdown component into explore.jsx

--- client/src/pages/explore.jsx
+++ client/src/pages/explore.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import Dropdown from "../components/Dropdown";
 
 const Explore = () => {
   return (
@@ -7,54 +8,7 @@ const Explore = () => {
       <div className="featured-craft">
         <h1 id="featured-craft">Flower Pots</h1>
       </div>
-      <div className="dropdown">
-        <button
-          className="btn btn-secondary dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Search Crafts
-        </button>
-
-        <ul className="dropdown-menu">
-          <li>
-            <a className="dropdown-item" href="#">
-              Crochet
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Knitting
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Painting
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Clay Modeling
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Jewelry
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Stained Glass
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Pottery
-            </a>
-          </li>
-        </ul>
-      </div>
+      <Dropdown />
     </div>
   );
 };
