a20d9e1c27fea864ca26ca94ab9cc29a9f283314
kaitlynmooney <kmooney806@gmail.com>
2024-08-09 12:52:45 -0400

    added search bar

--- client/src/pages/explore.jsx
+++ client/src/pages/explore.jsx
@@ -3,12 +3,25 @@ import { useState } from "react";
 const Explore = () => {
   return (
     <div id="explore">
-        <p>Featured Craft of the Week</p>
-        <div>
+      <p>Featured Craft of the Week</p>
+      <div className="featured-craft">
         <h1 id="featured-craft">Flower Pots</h1>
-        </div>
+      </div>
+      <input
+        class="form-control"
+        list="datalistOptions"
+        id="search-craft"
+        placeholder="Search for a craft"
+      ></input>
+      <datalist id="datalistOptions">
+        <option value="Jewelry Making"></option>
+        <option value="Sewing"></option>
+        <option value="Pottery"></option>
+        <option value="Painting"></option>
+        <option value="Stained Glass Making"></option>
+        <option value="Crocheting"></option>
+      </datalist>
     </div>
   );
 };
-
 export default Explore;
