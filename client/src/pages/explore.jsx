7e0219d1cae71e08dd457184a96b77a51c7eb14d
kaitlynmooney <kmooney806@gmail.com>
2024-08-09 14:50:32 -0400

    added craft sections

--- client/src/pages/explore.jsx
+++ client/src/pages/explore.jsx
@@ -9,6 +9,33 @@ const Explore = () => {
         <h1 id="featured-craft">Flower Pots</h1>
       </div>
       <Dropdown />
+      <div id="popular">
+        <p>Popular Crafts</p>
+        <button className="borders" id="popular-btn" type="submit">
+          <div>Pottery</div>
+        </button>
+        <button className="borders" id="popular-btn" type="submit">
+          <div>Crochet</div>
+        </button>
+      </div>
+      <div id="new">
+        <p>New Crafts</p>
+        <button className="borders" id="popular-btn" type="submit">
+          <div>Stained Glass</div>
+        </button>
+        <button className="borders" id="popular-btn" type="submit">
+          <div>Jewelry</div>
+        </button>
+      </div>
+      <div id="affordable">
+        <p>Affordable Crafts</p>
+        <button className="borders" id="popular-btn" type="submit">
+          <div>Knitting</div>
+        </button>
+        <button className="borders" id="popular-btn" type="submit">
+          <div>Clay Modeling</div>
+        </button>
+      </div>
     </div>
   );
 };
