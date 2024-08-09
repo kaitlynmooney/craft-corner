fafc915a31619918d4d7363fcfd2f26dac120be4
Megan McNeill <mcneim2@gmail.com>
2024-08-08 22:35:03 -0400

    added login functions

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,46 @@
 /* DEPENDENCIES */
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
 
 /* LOGIN PAGE */
-export default function LoginPage() {
+// export default function LoginPage() {
+const LoginPage = () => {
+  const [ formState, setFormState ] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+    // update state based on form input changes
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+  
+      setFormState({
+        ...formState,
+        [name]: value,
+      });
+    };
+
+      // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
   return (
     <div className="inter" id="login-page">
       <div>
@@ -88,4 +127,6 @@ export default function LoginPage() {
       </div>
     </div>
   );
-}
+};
+
+export default LoginPage;
