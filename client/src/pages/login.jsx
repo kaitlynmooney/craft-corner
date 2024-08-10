f5e68b14cd3ed1486dd52f300a824cb4160871c8
Megan McNeill <mcneim2@gmail.com>
2024-08-09 20:18:57 -0400

    added signup or login show conditionals

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,21 +1,35 @@
 /* DEPENDENCIES */
 import Login from '../components/login';
+import { useState } from 'react';
+import Signup from '../components/Signup';
 
 /* LOGIN PAGE */
 const LoginPage = () => {
+
+  const [ loginShow, setLoginShow ] = useState(true);
+  const [ signupShow, setSignupShow ] = useState(false);
   
   return (
     <div className="inter" id="login-page">
       <div>
-        <button className="no-borders" id="show-login-button">
+        <button 
+          className="no-borders" 
+          id="show-login-button" 
+          onClick={ () => {setLoginShow(true); setSignupShow(false)}}
+          >
           Log in
         </button>
-        <button className="no-borders" id="show-signup-button">
+        <button 
+          className="no-borders" 
+          id="show-signup-button"
+          onClick={ () => {setLoginShow(false); setSignupShow(true)}}
+          >
           Sign up
         </button>
       </div>
       <div id="login-main-div">
-        <Login />
+        {loginShow && <Login />}
+        {signupShow && <Signup />}
       </div>
       <div id="google-oauth-div">
         <p className="borders" id="google-oauth">
