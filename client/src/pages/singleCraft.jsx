4ccd677da491385b505d0b7f7dc29365615aa11c
Violet Stanziani <stanzianiviolet@gmail.com>
2024-08-12 14:11:55 -0400

    fixed images on individual project page

--- client/src/pages/singleCraft.jsx
+++ client/src/pages/singleCraft.jsx
@@ -1,82 +1,70 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-o
-import SingleProjHero from '../components/SingleProjHero';
 
-import { QUERY_SINGLE_PROJECT } from '../utils/queries'; 
+import SingleProjHero from "../components/SingleProjHero";
 
+import { QUERY_SINGLE_PROJECT } from "../utils/queries";
 
 const SingleProject = () => {
   const { projectId } = useParams();
 
-    const { loading, data } = useQuery(QUERY_SINGLE_PROJECT, {
-        variables : {projectId :projectId},
-    });
-    console.log(data)
-    // console.log(data?.project?.materials)
+  const { loading, data } = useQuery(QUERY_SINGLE_PROJECT, {
+    variables: { projectId: projectId },
+  });
+  console.log(data);
+  // console.log(data?.project?.materials)
 
   const project = data?.project || {};
 
-
-     if (loading) {
+  if (loading) {
     return <div className="loading-spinner"></div>;
   }
-    return(
+  return (
+    <div>
+        <SingleProjHero />
+        <span className="badge rounded-pill text-bg-info">{project.difficulty}</span>
+        <span className="badge rounded-pill text-bg-info">{project.pricePoint}</span>
+    <div className="container">
+      
+      
+
+      <div className="row">
+        <div className="col">
         
-        <div className="container">
-             
-            <SingleProjHero />
+          <img
+            className="project-image borders "
+            src={`/images/${project.image}`}
+            alt="Selected projects photo"
+          ></img>
+        </div>
 
-            
-
-            <div className="row">
-
-        
-
-
-          <div className="col">
-            <img className="project-image borders "  src={`/images/${project.image}`} alt="Selected projects photo"></img>
-          </div>
-
-          <div className="col order-1 ">
+        <div className="col order-1 ">
           <h1 className="title line-buffers">Materials</h1>
 
           {project.materials.map((material, index) => (
-            <ul className=" inter project-instructions materials-list" key ={index}>
-                <li><input type="checkbox"></input> {material}</li>
+            <ul
+              className=" inter project-instructions materials-list"
+              key={index}
+            >
+              <li>
+                <input type="checkbox"></input> {material}
+              </li>
             </ul>
-             ))}
-           <h2 className="title line-buffers">Instructions</h2>
+          ))}
+          <h2 className="title line-buffers">Instructions</h2>
 
-           {project.instructions.map((instruction, index) => (
-            <ul className="inter project-instructions materials-list" key={index}>
-                <li>{instruction}</li>
+          {project.instructions.map((instruction, index) => (
+            <ul
+              className="inter project-instructions materials-list"
+              key={index}
+            >
+              <li>{instruction}</li>
             </ul>
-
-           ))}
-           
-              
-
-          </div>
-          
-         
-
-          
-
-
-          
-
-
-
-    
-               
-            </div>
-           
-                
-               
-
-
-  
+          ))}
+        </div>
+      </div>
+    </div>
+    </div>
   );
 };
 
