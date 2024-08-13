c1d84e420eff1da7029f681883946d48e61c4122
kaitlynmooney <kmooney806@gmail.com>
2024-08-13 15:30:40 -0400

    added link styling

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,32 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import heroImage from "../images/flower-hero.jpg";
-import Projects from "../components/Projects";
-import { useQuery } from "@apollo/client";
-import { QUERY_ALL_PROJECTS_CRAFTS } from "../utils/queries";
+
 
 const Hero = () => {
-  const [category, setCategory] = useState("");
-  // query all projects
-  const {
-    data,
-  } = useQuery(QUERY_ALL_PROJECTS_CRAFTS);
-
-  const allProjects = data?.allProjects;
-
-  let filteredArray = allProjects || [];
-  if (category) {
-    filteredArray = allProjects?.filter(
-      (project) => project.craft.name === category
-    );
-  }
-
-  // category handler
-  const categoryHandler = (event) => {
-    event.preventDefault();
-    setCategory(event.target.innerText);
-  };
-
   return (
     <>
     <div className="hero">
@@ -38,7 +15,7 @@ const Hero = () => {
       <div className="hero-content">
         <p id="explore-feature">Featured Craft of the Week</p>
         <div className="featured-craft">
-          <Link to="/project/" onClick={categoryHandler} id="featured-craft" role="button">
+          <Link  to="/project/ "id="featured-craft" role="button">
             Flower Bouquets
           </Link>
         </div>
