05bdf8ea702c92717e06030119f90f9d80f0a720
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-08-13 16:09:05 -0400

    fix: error checking on dashboard

--- client/src/components/SavedProjects.jsx
+++ client/src/components/SavedProjects.jsx
@@ -1,70 +1,88 @@
 import { useDrag } from "react-dnd";
-import { ItemTypes } from '../utils/itemTypes';
-import { useNavigate } from 'react-router-dom';
+import { ItemTypes } from "../utils/itemTypes";
+import { useNavigate } from "react-router-dom";
 
-const SavedProjects = ({ savedProjects, allProjects, setSavedProjects, handleDropProject }) => {
-    const handleRemoveProject = (projectId) => {
-        // Remove the project ID from saved projects state
-        const updatedProjects = savedProjects.filter((_id) => _id !== projectId);
-        setSavedProjects(updatedProjects);
+const SavedProjects = ({
+  savedProjects,
+  allProjects,
+  setSavedProjects,
+  handleDropProject,
+}) => {
+  const handleRemoveProject = (projectId) => {
+    // Remove the project ID from saved projects state
+    const updatedProjects = savedProjects.filter((_id) => _id !== projectId);
+    setSavedProjects(updatedProjects);
 
-        // Update local storage to remove the project ID
-        const storedProjectIds = JSON.parse(localStorage.getItem('checkedProjectIds')) || [];
-        const indexToRemove = storedProjectIds.indexOf(projectId);
-        if (indexToRemove !== -1) {
-            storedProjectIds.splice(indexToRemove, 1);
-            localStorage.setItem('checkedProjectIds', JSON.stringify(storedProjectIds));
-        }
-    };
+    // Update local storage to remove the project ID
+    const storedProjectIds =
+      JSON.parse(localStorage.getItem("checkedProjectIds")) || [];
+    const indexToRemove = storedProjectIds.indexOf(projectId);
+    if (indexToRemove !== -1) {
+      storedProjectIds.splice(indexToRemove, 1);
+      localStorage.setItem(
+        "checkedProjectIds",
+        JSON.stringify(storedProjectIds)
+      );
+    }
+  };
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleDivClick = (projectId, event) => {
-        if (!event.target.classList.contains('btn-close')) {
-            // Navigate to the project details page if the close button was not clicked
-            navigate(`/project/${projectId}`);
-        }
-    };
+  const handleDivClick = (projectId, event) => {
+    if (!event.target.classList.contains("btn-close")) {
+      // Navigate to the project details page if the close button was not clicked
+      navigate(`/project/${projectId}`);
+    }
+  };
 
-    // Define a draggable project component
-    const DraggableProject = ({ project }) => {
-        console.log(ItemTypes.PROJECT);
-        const [{ isDragging }, drag] = useDrag({
-            type: 'PROJECT',
-            item: { project },
-            collect: (monitor) => ({
-                isDragging: !!monitor.isDragging(),
-            }),
-        });
+  // Define a draggable project component
+  const DraggableProject = ({ project }) => {
+    if (project && project.name) {
+      console.log(ItemTypes.PROJECT);
+      const [{ isDragging }, drag] = useDrag({
+        type: "PROJECT",
+        item: { project },
+        collect: (monitor) => ({
+          isDragging: !!monitor.isDragging(),
+        }),
+      });
 
-        const handleDrop = () => {
-            handleDropProject(); // Call handleDropProject when a project is dropped
-        };
+      const handleDrop = () => {
+        handleDropProject(); // Call handleDropProject when a project is dropped
+      };
 
-        return (
-            <div ref={drag} style={{ opacity: isDragging ? 0.5 : 1 }}>
-                <div onClick={(event) => handleDivClick(project._id, event)}>
-                    <button className='button-options'>
-                        <button type="button" className="btn-close" aria-label="Close" onClick={() => handleRemoveProject(project._id)}></button>
-                        <label id='label'>{project.name}</label>
-                    </button>
-                </div>
-            </div>
-        );
-    };
-
-    return (
-        <div id="project-container">
-            {savedProjects && savedProjects.length > 0 ? (
-                savedProjects.map((projectId) => (
-                    <DraggableProject key={projectId} project={allProjects.find((p) => p._id === projectId)} />
-                ))
-            ) : (
-                <h4>No saved projects yet!</h4>
-            )}
+      return (
+        <div ref={drag} style={{ opacity: isDragging ? 0.5 : 1 }}>
+          <div onClick={(event) => handleDivClick(project._id, event)}>
+            <button className="button-options">
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+                onClick={() => handleRemoveProject(project._id)}
+              ></button>
+              <label id="label">{project.name}</label>
+            </button>
+          </div>
         </div>
-    );
+      );
+    }
+  };
+
+  return (
+    <div id="project-container">
+      {savedProjects && savedProjects.length > 0 ? (
+        savedProjects.map((projectId) => (
+          <DraggableProject
+            key={projectId}
+            project={allProjects.find((p) => p._id === projectId)}
+          />
+        ))
+      ) : (
+        <h4>No saved projects yet!</h4>
+      )}
+    </div>
+  );
 };
 
 export default SavedProjects;
-
